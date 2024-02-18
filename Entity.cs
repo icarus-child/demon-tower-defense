9098fbe50cf5dc1d363a8a60577b5ab589b68902
Ethan Meier <e652@hotmail.ca>
2024-02-17 22:30:41 -0800

    Entity Attack Animation

--- Entity.cs
+++ Entity.cs
@@ -12,58 +12,85 @@ public partial class Entity : CharacterBody2D
 	}
 
 	[Export] public Team EntityTeam { get; set; }
-	[Export] private int _health;
-	[Export] private int _damage;
-	[Export] private bool _attackRange;
-	[Export] private bool _attackCooldown;
+	[Export] private float _health;
+	[Export] private float _damage;
+	[Export] private double _attackCooldown;
 	[Export] private bool _doesAoe;
-	[Export] private bool _aoeRange;
-	[Export] private int _wallMultiplier;
-	[Export] private int _enemyMultiplier;
-	[Export] private float speed = 35;
+	[Export] private float _aoeRange;
+	[Export] private float _wallMultiplier;
+	[Export] private float _enemyMultiplier;
+	[Export] private float speed;
 
 	[Export]
 	public Node2D Portal;
 	public Node2D Target;
-	private NavigationAgent2D navigationAgent;
-	private Area2D aggroRange;
-	private Area2D attackRange;
-	private readonly List<Node2D> targetOptions = new();
+	private NavigationAgent2D _navigationAgent;
+	private Area2D _aggroRange;
+	private Area2D _attackRange;
+	private readonly List<Node2D> _targetOptions = new();
 	private AnimatedSprite2D _sprite;
+	private Timer _attackCooldownTimer;
+	private bool _canAttack = false;
 	
     public override void _Ready()
     {
-		navigationAgent = GetNode<NavigationAgent2D>("NavigationAgent2D");
-		aggroRange = GetNode<Area2D>("AggroRange");
-		attackRange = GetNode<Area2D>("AttackRange");
-		aggroRange.BodyEntered += body => {
-			targetOptions.Add(body);
+        _attackCooldownTimer = new()
+        {
+            OneShot = true,
+            WaitTime = _attackCooldown,
+			Autostart = true
+        };
+		AddChild(_attackCooldownTimer);
+		_navigationAgent = GetNode<NavigationAgent2D>("NavigationAgent2D");
+		_aggroRange = GetNode<Area2D>("AggroRange");
+		_attackRange = GetNode<Area2D>("AttackRange");
+		_aggroRange.BodyEntered += body => {
+			_targetOptions.Add(body);
 		};
-		aggroRange.BodyExited += body => {
-			targetOptions.Remove(body);
+		_aggroRange.BodyExited += body => {
+			_targetOptions.Remove(body);
 		};
-		_sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-		_sprite.Play("walk");
+		_attackCooldownTimer.Timeout += () => {
+			_canAttack = true;
+		};
+        _sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		_sprite.Play("idle");
     }
 
     public override void _Process(double delta)
     {
-	    if (Target == null) return;
+		if (Target is null) {
+			if (_targetOptions.Count != 0) Target = GetClosestTargetable();
+			else Target = Portal;
+		}
 
 	    // Make this not shit later
 	    try {
-		    int i = navigationAgent.GetCurrentNavigationPathIndex() - 2;
-		    _sprite.FlipH = navigationAgent.GetCurrentNavigationPath()[i].X > ToGlobal(Position).X; // Make sprite face the target
+		    int i = _navigationAgent.GetCurrentNavigationPathIndex() - 2;
+		    _sprite.FlipH = _navigationAgent.GetCurrentNavigationPath()[i].X > ToGlobal(Position).X; // Make sprite face the target
 	    } catch (IndexOutOfRangeException e) {}
 
     }
 
 	public override void _PhysicsProcess(double delta)
 	{
-		if (Target is null) return;
-		if (!attackRange.OverlapsBody(Target))
+		if (Target is null && _targetOptions.Count == 1)
 		{
+			Target = GetClosestTargetable();
+		} else if (Target is null) {
+			return;
+		}
+
+		if (!_attackRange.OverlapsBody(Target))
+		{
+			GD.Print("AttackMove");
 			AttackMove();
+		} else {
+			// Attack
+			if (_canAttack) {
+				GD.Print("Attack");
+				_sprite.Play("attack");
+			}
 		}
 	}
 
@@ -71,31 +98,32 @@ public partial class Entity : CharacterBody2D
 	private void Path()
 	{
 		// Pathing
-		if (navigationAgent.IsNavigationFinished()) return;
-		Vector2 direction = ToLocal(navigationAgent.GetNextPathPosition()).Normalized();
+		if (_navigationAgent.IsNavigationFinished()) return;
+		Vector2 direction = ToLocal(_navigationAgent.GetNextPathPosition()).Normalized();
 		Velocity = direction * speed;
 		MoveAndSlide();
+		_sprite.Play("walk");
 	}
 
 	private void AttackMove()
 	{
-		if ((Target == Portal || Target is null) && targetOptions.Count != 0) Target = GetClosestTargetable();
+		if (_targetOptions.Count != 0) Target = GetClosestTargetable();
 		Path();
 	}
 	
+	private void Move()
+	{
+		Path();
+	}
+
 	private void CreatePath()
-	{	
-		// Target Reaquisition
-		if (Target is null) {
-			if (targetOptions.Count != 0) Target = GetClosestTargetable();
-			else Target = Portal;
-		}
-		navigationAgent.TargetPosition = Target.GlobalPosition;
+	{
+		_navigationAgent.TargetPosition = Target.GlobalPosition;
 	}
 
 	private Node2D GetClosestTargetable()
 	{
-		return targetOptions.MinBy(node =>
+		return _targetOptions.MinBy(node =>
 		{
 			return node.GlobalPosition.DistanceTo(GlobalPosition);
 		});
